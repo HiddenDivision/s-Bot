4da62a98c1b160ea19e5da2591b9e70532ae79ac
HiddenDivision <52402557+HiddenDivision@users.noreply.github.com>
2019-08-10 17:09:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,99 +14,99 @@ bot.on('message', msg=>{
     }
     let args = msg.content.substring(testpre.length - 1).split(" ");
 	switch(args[0]){
-		case '-Nig':
-            if(msg.author.id === '333357946744602647'){
-			    const aMessage = args.join(" ").slice(5);
-    			const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
-    			const embed = new RichEmbed()
-                .setTitle(aMessage)
-                .setColor(0x160033)
-                 msg.channel.sendEmbed(embed);
-            }
-            if(msg.author.id === '601697629000433664'){
-                const aMessage = args.join(" ").slice(5);
-                const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
-                const embed = new RichEmbed()
-                .setTitle(aMessage)
-                .setColor(0x160033)
-                msg.channel.sendEmbed(embed);
-            }
-            if(msg.author.id === '345322541499285507'){
-                const aMessage = args.join(" ").slice(5);
-                const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
-                const embed = new RichEmbed()
-                .setTitle(aMessage)
-                .setColor(0x160033)
-                msg.channel.sendEmbed(embed);
-            }
-            if(msg.author.id === '599661787608121375'){
-                const aMessage = args.join(" ").slice(5);
-                const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
-                const embed = new RichEmbed()
-                .setTitle(aMessage)
-                .setColor(0x160033)
-                msg.channel.sendEmbed(embed);
-            }
-            if(msg.author.id === '308229212656107521'){
-                const aMessage = args.join(" ").slice(5);
-                const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
-                const embed = new RichEmbed()
-                .setTitle(aMessage)
-                .setColor(0x160033)
-                msg.channel.sendEmbed(embed);
-            }
-        break;
-        case 'Hidden.':
-            if(msg.author.id === '333357946744602647'){
-    			const agif = new Attachment('https://media.discordapp.net/attachments/598945838646951956/601251574857203713/Hidden.gif');
-                msg.channel.send(agif);
-           }
-        break;
-        case 'HD.':
-            if(msg.author.id === '333357946744602647'){
-    			const embed = new RichEmbed()
-	        	.setTitle('The Hidden Division.')
-                .setDescription("​\n\nThe Hidden Division is here to recruit skilled and hidden members. Your power and strength will be helpful to us\n\nTo join the Hidden Division, Please contact Fridy#8811 through his DM's\n\n\n-The Hidden Division")
-                .setThumbnail('https://media.discordapp.net/attachments/598945838646951956/601251574857203713/Hidden.gif')
-	        	.setColor(0x160033)
-	        	msg.channel.sendEmbed(embed);
-           }
-        break;
-        case "I'm":
-            if(args[1] === 'hidden.'){
-			const embed2 = new RichEmbed()
-            .setTitle('Stay hidden then.')
-            .setColor(0x160033)
-            msg.channel.sendEmbed(embed2);
-            }
-        break;
-	case 'dmallthosenigsbciwant':
-		if(msg.author.id === '609301072766566400'){
-		if(!args[1]) return msg.channel.sendMessage('?');
-        	let dmGuild = msg.guild;
-			var message = msg.content.slice(22);
-			if(!dmGuild.members) return msg.channel.send("Nigga I can't do that in DM's");
-			let memberarray = dmGuild.members.array();
-			let membercount = memberarray.length;
-			console.log(`Responding to ${msg.author.username} :  Sending message to all ${membercount} members of ${dmGuild.name}.`)
-			for (var i = 0; i < membercount; i++) {
-				if(!args[1]) return msg.channel.sendMessage('?');
-				let timeout = Math.floor((Math.random() * (10 - 0.01)) * 1000) + 10;
-				let member = memberarray[i];
-				sleep(timeout)
-				if(i == (membercount-1)) {
-					console.log(`Waited ${timeout}ms.\t\\/\tDMing ${member.user.username}`);
-				} else {
-					console.log(`Waited ${timeout}ms.\t|${i + 1}|\tDMing ${member.user.username}`);
-                }
-                member.send(`${message}`);
-			}
-		}
-		break;
-		case 'Wigga':
+	//	case '-Nig':
+        //    if(msg.author.id === '333357946744602647'){
+	//		    const aMessage = args.join(" ").slice(5);
+    	//		const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
+    	//		const embed = new RichEmbed()
+        //        .setTitle(aMessage)
+        //        .setColor(0x160033)
+        //         msg.channel.sendEmbed(embed);
+        //    }
+        //    if(msg.author.id === '601697629000433664'){
+        //        const aMessage = args.join(" ").slice(5);
+        //        const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
+        //        const embed = new RichEmbed()
+        //        .setTitle(aMessage)
+        //        .setColor(0x160033)
+        //        msg.channel.sendEmbed(embed);
+        //    }
+        //    if(msg.author.id === '345322541499285507'){
+        //        const aMessage = args.join(" ").slice(5);
+        //        const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
+        //        const embed = new RichEmbed()
+        //        .setTitle(aMessage)
+        //        .setColor(0x160033)
+        //        msg.channel.sendEmbed(embed);
+        //    }
+        //    if(msg.author.id === '599661787608121375'){
+        //        const aMessage = args.join(" ").slice(5);
+        //        const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
+        //        const embed = new RichEmbed()
+        //        .setTitle(aMessage)
+        //        .setColor(0x160033)
+        //        msg.channel.sendEmbed(embed);
+        //    }
+        //    if(msg.author.id === '308229212656107521'){
+        //        const aMessage = args.join(" ").slice(5);
+        //        const agif = new Attachment('https://media.discordapp.net/attachments/572096391149649920/572508265506668556/Hidden_Division.gif');
+        //        const embed = new RichEmbed()
+        //        .setTitle(aMessage)
+        //        .setColor(0x160033)
+        //        msg.channel.sendEmbed(embed);
+        //    }
+        //break;
+        //case 'Hidden.':
+        //    if(msg.author.id === '333357946744602647'){
+    	//		const agif = new Attachment('https://media.discordapp.net/attachments/598945838646951956/601251574857203713/Hidden.gif');
+        //        msg.channel.send(agif);
+        //   }
+        //break;
+        //case 'HD.':
+        //    if(msg.author.id === '333357946744602647'){
+    	//		const embed = new RichEmbed()
+	//        	.setTitle('The Hidden Division.')
+        //        .setDescription("​\n\nThe Hidden Division is here to recruit skilled and hidden members. Your power and strength will be helpful to us\n\nTo join the Hidden Division, Please contact Fridy#8811 through his DM's\n\n\n-The Hidden Division")
+        //        .setThumbnail('https://media.discordapp.net/attachments/598945838646951956/601251574857203713/Hidden.gif')
+	//        	.setColor(0x160033)
+	//        	msg.channel.sendEmbed(embed);
+        //   }
+        //break;
+        //case "I'm":
+        //    if(args[1] === 'hidden.'){
+	//		const embed2 = new RichEmbed()
+        //    .setTitle('Stay hidden then.')
+        //    .setColor(0x160033)
+        //    msg.channel.sendEmbed(embed2);
+        //    }
+        //break;
+	//case 'dmallthosenigsbciwant':
+	//	if(msg.author.id === '609301072766566400'){
+	//	if(!args[1]) return msg.channel.sendMessage('?');
+        //	let dmGuild = msg.guild;
+	//		var message = msg.content.slice(22);
+	//		if(!dmGuild.members) return msg.channel.send("Nigga I can't do that in DM's");
+	//		let memberarray = dmGuild.members.array();
+	//		let membercount = memberarray.length;
+	//		console.log(`Responding to ${msg.author.username} :  Sending message to all ${membercount} members of ${dmGuild.name}.`)
+	//		for (var i = 0; i < membercount; i++) {
+	//			if(!args[1]) return msg.channel.sendMessage('?');
+	//			let timeout = Math.floor((Math.random() * (10 - 0.01)) * 1000) + 10;
+	//			let member = memberarray[i];
+	//			sleep(timeout)
+	//			if(i == (membercount-1)) {
+	//				console.log(`Waited ${timeout}ms.\t\\/\tDMing ${member.user.username}`);
+	//			} else {
+	//				console.log(`Waited ${timeout}ms.\t|${i + 1}|\tDMing ${member.user.username}`);
+        //        }
+        //        member.send(`${message}`);
+	//		}
+	//	}
+	//	break;
+		case 'HD':
             if(args[1] === 'slap'){
                 let ment = msg.mentions.users.first()
-                //let message = args.join(" ").slice(33);
+                let message = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to slap?')
                 }
@@ -120,7 +120,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'kiss'){
                 let ment = msg.mentions.users.first()
-                let message2 = args.join(" ").slice(33);
+                let message2 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to kiss?')
                 }
@@ -134,7 +134,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'fuck'){
                 let ment = msg.mentions.users.first()
-                let message3 = args.join(" ").slice(33);
+                let message3 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to... Nothing!')
                 }
@@ -148,7 +148,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'fu'){
                 let ment = msg.mentions.users.first()
-                let message4 = args.join(" ").slice(31);
+                let message4 = args.join(" ").slice(28);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to tell to fuck theirself?')
                 }
@@ -162,7 +162,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'kill'){
                 let ment = msg.mentions.users.first()
-                let message5 = args.join(" ").slice(33);
+                let message5 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to kill?')
                 }
@@ -176,7 +176,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'hug'){
                 let ment = msg.mentions.users.first()
-                let message6 = args.join(" ").slice(32);
+                let message6 = args.join(" ").slice(29);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to hug?')
                 }
@@ -190,7 +190,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'shoot'){
                 let ment = msg.mentions.users.first()
-                let message7 = args.join(" ").slice(34);
+                let message7 = args.join(" ").slice(31);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to shoot!?')
                 }
@@ -204,7 +204,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'kms'){
                 let ment = msg.mentions.users.first()
-                let message8 = args.join(" ").slice(32);
+                let message8 = args.join(" ").slice(29);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to kill yourself with..?')
                 }
@@ -218,7 +218,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'boop'){
                 let ment = msg.mentions.users.first()
-                let message9 = args.join(" ").slice(33);
+                let message9 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to boop?')
                 }
@@ -232,7 +232,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'suck'){
                 let ment = msg.mentions.users.first()
-                let message9 = args.join(" ").slice(33);
+                let message9 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to... WHY??')
                 }
@@ -246,7 +246,7 @@ bot.on('message', msg=>{
             }
             if(args[1] === 'lick'){
                 let ment = msg.mentions.users.first()
-                let message9 = args.join(" ").slice(33);
+                let message9 = args.join(" ").slice(30);
                 if(ment === undefined){
                     msg.channel.send('Who are you trying to... lick..?')
                 }
@@ -256,6 +256,35 @@ bot.on('message', msg=>{
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609649685476540416/tenor_10.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
+                }
+            }
+            if(args[1] === 'pat'){
+                let ment = msg.mentions.users.first()
+                let message9 = args.join(" ").slice(29);
+                if(ment === undefined){
+                    msg.channel.send('Who are you trying to pat?')
+                }
+                else{
+                    const embed9 = new RichEmbed()
+                    .setTitle(`${msg.author.username.toString()} pats ${ment.username}! ${message9}`)
+                    .setImage(`https://cdn.discordapp.com/attachments/609302155253383170/609794873335480350/tenor.gif`)
+                    .setColor(0x160033)
+                    msg.channel.sendEmbed(embed9)
+                }
+            }
+            if(args[1] === 'suicide'){
+                if(args[2] === 'bomb'){
+                    if(args[3] === '@everyone'){
+                        let message9 = args.join(" ").slice(26);
+                        const embed9 = new RichEmbed()
+                        .setTitle(`${msg.author.username.toString()} suicide bombed everyone! ${message9}`)
+                        .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609805383065927690/FXeaECZ.gif`)
+                        .setColor(0x160033)
+                        msg.channel.sendEmbed(embed9)
+                    }
+                    else{
+                        msg.channel.send('Who are you are you trying to suicide bomb?')
+                    }
                 }
             }
         break;
