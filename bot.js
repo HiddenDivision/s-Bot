8a8d3035dcc508725a21da832d3ecf7a9f45a63e
HiddenDivision <52402557+HiddenDivision@users.noreply.github.com>
2019-08-10 22:47:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,7 +112,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} slapped ${ment.username}! ${message}`)
+                    .setAuthor(`${msg.author.username.toString()} slapped ${ment.username}! ${message}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609624915326533645/tenor.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed)
@@ -126,7 +126,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed2 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} kissed ${ment.username}! ${message2}`)
+                    .setAuthor(`${msg.author.username.toString()} kissed ${ment.username}! ${message2}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609625749959475230/tenor_1.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed2)
@@ -140,7 +140,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed3 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} is fucking... ${ment.username}... Gosh why? ${message3}`)
+                    .setAuthor(`${msg.author.username.toString()} is fucking... ${ment.username}... Gosh why? ${message3}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/609302155253383170/609628232526069766/tenor_2.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed3)
@@ -154,9 +154,9 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed4 = new RichEmbed()
-                    .setTitle(`${ment.username}, go fuck yourself.  ${msg.author.username.toString()} doesn't like you.  ${message4}`)
                     .setImage(`https://cdn.discordapp.com/attachments/609302155253383170/609629429953724416/tenor_3.gif`)
                     .setColor(0x160033)
+                    .setAuthor(`${ment.username}, go fuck yourself.  ${msg.author.username.toString()} doesn't like you.  ${message4}`, `${msg.author.avatarURL}`, '')
                     msg.channel.sendEmbed(embed4)
                 }
             }
@@ -168,7 +168,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed5 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} killed ${ment.username}! ${message5}`)
+                    .setAuthor(`${msg.author.username.toString()} killed ${ment.username}! ${message5}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/609302155253383170/609631380586102784/tenor_4.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed5)
@@ -182,7 +182,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed6 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} hugged ${ment.username}! ${message6}`)
+                    .setAuthor(`${msg.author.username.toString()} hugged ${ment.username}! ${message6}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609637268906311707/tenor_1.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed6)
@@ -196,7 +196,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed7 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} shot ${ment.username}! ${message7}`)
+                    .setAuthor(`${msg.author.username.toString()} shot ${ment.username}! ${message7}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609643152260726785/tenor_6.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed7)
@@ -210,7 +210,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed8 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} goes kill himself with ${ment.username}... ${message8}`)
+                    .setAuthor(`${msg.author.username.toString()} goes kill himself with ${ment.username}... ${message8}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609647462264864779/tenor_7.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed8)
@@ -224,7 +224,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} booped ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} booped ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609648336638705665/tenor_8.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -238,7 +238,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} is sucking... ${ment.username}... Why am I part of this? ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} is sucking... ${ment.username}... Why am I part of this? ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609648493434503193/tenor_9.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -252,7 +252,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} is licking... ${ment.username}... So lewd... ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} is licking... ${ment.username}... So lewd... ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/570053851315568650/609649685476540416/tenor_10.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -266,7 +266,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} pats ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} pats ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/609302155253383170/609794873335480350/tenor.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -277,7 +277,7 @@ bot.on('message', msg=>{
                     if(args[3] === '@everyone'){
                         let message9 = args.join(" ").slice(26);
                         const embed9 = new RichEmbed()
-                        .setTitle(`${msg.author.username.toString()} suicide bombed everyone! ${message9}`)
+                        .setAuthor(`${msg.author.username.toString()} suicide bombed everyone! ${message9}`, `${msg.author.avatarURL}`, '')
                         .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609805383065927690/FXeaECZ.gif`)
                         .setColor(0x160033)
                         msg.channel.sendEmbed(embed9)
@@ -295,7 +295,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} cuddles ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} cuddles ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609908895661424651/tenor.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -309,7 +309,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} tickles ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} tickles ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609909154781593610/tenor_1.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -323,7 +323,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} is peeping on ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} is peeping on ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609910538969022494/tenor_2.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -338,7 +338,7 @@ bot.on('message', msg=>{
                     }
                     else{
                         const embed9 = new RichEmbed()
-                        .setTitle(`${msg.author.username.toString()} ass raped ${ment.username}! ${message9}`)
+                        .setAuthor(`${msg.author.username.toString()} ass raped ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                         .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609913346908618775/tenor_3.gif`)
                         .setColor(0x160033)
                         msg.channel.sendEmbed(embed9)
@@ -353,7 +353,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} kidnapped ${ment.username}! ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} kidnapped ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609915218658852865/tenor_4.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -367,7 +367,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} is going to rape ${ment.username}... Oh no. ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} is going to rape ${ment.username}... Oh no. ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609917249234403360/5e4.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -381,7 +381,7 @@ bot.on('message', msg=>{
                 }
                 else{
                     const embed9 = new RichEmbed()
-                    .setTitle(`${msg.author.username.toString()} wants to lewd ${ment.username}... WHY??? ${message9}`)
+                    .setAuthor(`${msg.author.username.toString()} wants to lewd ${ment.username}... WHY??? ${message9}`, `${msg.author.avatarURL}`, '')
                     .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609918047074910228/tenor_5.gif`)
                     .setColor(0x160033)
                     msg.channel.sendEmbed(embed9)
@@ -397,7 +397,7 @@ bot.on('message', msg=>{
                         }
                         else{
                             const embed9 = new RichEmbed()
-                            .setTitle(`${msg.author.username.toString()} is FBI opening up ${ment.username}! ${message9}`)
+                            .setAuthor(`${msg.author.username.toString()} is FBI opening up ${ment.username}! ${message9}`, `${msg.author.avatarURL}`, '')
                             .setImage(`https://cdn.discordapp.com/attachments/602964327431012513/609918828260098079/tenor_6.gif`)
                             .setColor(0x160033)
                             msg.channel.sendEmbed(embed9)
